386b4b39578600bb54528df2f747ee9372cd1e80
angusdev <angusdev@d7ae0720-9a48-0410-a770-9b81666e587d>
2009-02-17 12:52:36 +0000

    hellotxt should return immediate if it is a tinyurl purpose

--- trunk/better-mobile-twitter/bettermobiletwitter.user.js
+++ trunk/better-mobile-twitter/bettermobiletwitter.user.js
@@ -281,6 +281,7 @@ BetterMobileTwitter.prototype.expandUrl_image = function(a, url, imgsrc) {
 BetterMobileTwitter.prototype.expandUrl_hellotxt = function(bmt, a, url, t) {
   if (url != t.finalUrl) {
     bmt.expandUrl_tinyurl(bmt, a, url, t);
+    return;
   }
 
   t = bmt.extract(bmt.extract(t.responseText, '<div class="history-row big">'), '<p>', '</p>');
